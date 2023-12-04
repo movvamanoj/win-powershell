10dd5fd1d6abe96ff613731dd63b50c482d89b6b
movvamanoj <movvamanoj@gmail.com>
2023-12-04 18:19:25 +0530

    changes in win-disk-l-n v4

--- ansible/win-disk-l-n.ps1
+++ ansible/win-disk-l-n.ps1
@@ -1,4 +1,4 @@
-# Function to get the next available drive letter #v3
+# Function to get the next available drive letter
 function Get-NextAvailableDriveLetter {
     $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
     $alphabet = [char[]]('D'..'Z')
@@ -23,10 +23,24 @@ foreach ($diskNumber in $diskNumbers) {
     $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
 
     # Check if the drive letter is already in use for the disk
-    if (Test-Path "$nextAvailableDriveLetter`:") {
+    $driveLetterInUse = Get-Volume -DriveLetter $nextAvailableDriveLetter -ErrorAction SilentlyContinue
+
+    if ($driveLetterInUse) {
         Write-Host "Drive letter $nextAvailableDriveLetter is already in use for Disk $diskNumber. Skipping partition creation."
     }
     else {
+        # Check if the disk is not online or not initialized
+        $disk = Get-Disk -Number $diskNumber
+        if ($disk.IsOffline -or ($disk.PartitionStyle -eq 'RAW')) {
+            # Initialize the disk with GPT partition style
+            Initialize-Disk -Number $diskNumber -PartitionStyle GPT
+            Write-Host "Disk $diskNumber initialized."
+        }
+        else {
+            Write-Host "Disk $diskNumber is already initialized. Skipping initialization."
+        }
+
+        # Create a new partition with the dynamically assigned drive letter
         New-Partition -DiskNumber $diskNumber -UseMaximumSize -AssignDriveLetter | Out-Null
         $driveLetter = (Get-Partition -DiskNumber $diskNumber).DriveLetter
         Write-Host "Partition on Disk $diskNumber created with dynamically assigned drive letter $driveLetter."
@@ -37,7 +51,7 @@ foreach ($diskNumber in $diskNumbers) {
     }
 }
 
-# Format the volumes with the NTFS file system
+# Format the volumes with the NTFS file system  
 foreach ($diskNumber in $diskNumbers) {
     # Get the dynamically assigned drive letter of the formatted partition
     $driveLetter = (Get-Partition -DiskNumber $diskNumber).DriveLetter
