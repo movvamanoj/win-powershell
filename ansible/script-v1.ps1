51dce590a895ea25d4a1eecc65d7f6ab6a5f0a68
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-05 22:33:29 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -1,45 +1,65 @@
-# Step 1: Initialization - Get a list of disks with attached volumes and initialize them if needed
-$attachedDisks = Get-Disk | Where-Object { $_.IsOffline -eq $false -and $_.PartitionStyle -ne 'RAW' }
-
-foreach ($disk in $attachedDisks) {
-    $diskNumber = $disk.Number
-
-    # Check if the disk is not online or not initialized
-    if ($disk.IsOffline -or ($disk.PartitionStyle -eq 'RAW')) {
-        # Initialize the disk with GPT partition style
-        Initialize-Disk -Number $diskNumber -PartitionStyle GPT
-        Write-Host "Disk $diskNumber initialized."
-    }
-    else {
-        Write-Host "Disk $diskNumber is already initialized. Skipping initialization."
+# Check if any disk has already been allocated a drive letter
+$allocatedDisks = Get-Disk | Where-Object { $_.IsOffline -eq $false -and $_.PartitionStyle -ne 'RAW' } | ForEach-Object {
+    $volume = Get-Partition -DiskNumber $_.Number | Get-Volume
+    if ($volume.DriveLetter -ne $null) {
+        $_.Number
     }
 }
 
-# Step 2: Formatting - Format the volumes with NTFS file system
+if ($allocatedDisks.Count -gt 0) {
+    Write-Host "Skipping the process as the following disks already have allocated drive letters: $($allocatedDisks -join ', ')"
+}
+else {
+    # Step 1: Initialization - Get a list of disks with attached volumes and initialize them if needed
+    $attachedDisks = Get-Disk | Where-Object { $_.IsOffline -eq $false -and $_.PartitionStyle -ne 'RAW' }
+
+    foreach ($disk in $attachedDisks) {
+        $diskNumber = $disk.Number
+
+        # Check if the disk is not online or not initialized
+        if ($disk.IsOffline -or ($disk.PartitionStyle -eq 'RAW')) {
+            # Initialize the disk with GPT partition style
+            Initialize-Disk -Number $diskNumber -PartitionStyle GPT
+            Write-Host "Disk $diskNumber initialized."
+        }
+        else {
+            Write-Host "Disk $diskNumber is already initialized. Skipping initialization."
+        }
+    }
+    
+# Step 2: Formatting - Format the volumes with NTFS file system for disks that are initialized
 foreach ($disk in $attachedDisks) {
     $diskNumber = $disk.Number
     $volume = Get-Partition -DiskNumber $diskNumber | Get-Volume
 
-    # Format the volume with NTFS file system
-    Format-Volume -DriveLetter $volume.DriveLetter -FileSystem NTFS -NewFileSystemLabel "SC1CALL" -AllocationUnitSize 65536 -ErrorAction Stop
-    Write-Host "Volume on Disk $diskNumber formatted and labeled."
+    # Check if the volume has a drive letter and is formatted
+    if ($volume.DriveLetter -ne $null -and $volume.FileSystem -eq 'NTFS') {
+        Write-Host "Volume on Disk $diskNumber is already formatted. Skipping formatting."
+    }
+    else {
+        # Format the volume with NTFS file system
+        Format-Volume -DriveLetter $volume.DriveLetter -FileSystem NTFS -NewFileSystemLabel "SC1CALL" -AllocationUnitSize 65536 -ErrorAction Stop
+        Write-Host "Volume on Disk $diskNumber formatted and labeled."
+    }
 }
 
-# Step 3: Drive Letter Allocation - Allocate drive letters dynamically
+# Step 3: Drive Letter Allocation - Allocate drive letters dynamically for disks that are initialized
 $nextAvailableDriveLetters = @()
 
 foreach ($disk in $attachedDisks) {
     $diskNumber = $disk.Number
-    $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
-    $nextAvailableDriveLetters += $nextAvailableDriveLetter
+    $volume = Get-Partition -DiskNumber $diskNumber | Get-Volume
 
-    if (Test-DriveLetterInUse -DriveLetter $nextAvailableDriveLetter) {
-        Write-Host "Drive letter $nextAvailableDriveLetter is already in use for Disk $diskNumber. Skipping allocation."
+    # Check if the volume has a drive letter
+    if ($volume.DriveLetter -ne $null) {
+        Write-Host "Drive letter $($volume.DriveLetter) is already allocated for Disk $diskNumber. Skipping allocation."
     }
     else {
+        $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
+
         # Assign the drive letter to the volume
-        $volume = Get-Partition -DiskNumber $diskNumber | Get-Volume
         $volume | Set-Volume -NewDriveLetter $nextAvailableDriveLetter
         Write-Host "Drive letter $nextAvailableDriveLetter allocated for Disk $diskNumber."
     }
+ }
 }
