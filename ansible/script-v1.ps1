02ebe9252425239b15d79878a8519b1daa317fef
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-05 20:47:29 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -25,67 +25,31 @@ function Test-DriveLetterInUse {
     return $usedDriveLetters -contains $DriveLetter
 }
 
-# Check if each disk is already initialized and has a drive letter
+# Create a new partition on each disk with specific drive letters and format volumes
 foreach ($diskNumber in $diskNumbers) {
     # Skip Disk 0 (OS disk)
     if ($diskNumber -eq 0) {
-        Write-Host "Skipping initialization for Disk 0 (OS disk)."
+        Write-Host "Skipping partition creation for Disk 0 (OS disk)."
         continue
     }
-
-    $disk = Get-Disk -Number $diskNumber
-
-    # Skip if the disk is already initialized or has a drive letter
-    if ($disk.IsOffline -or ($disk.PartitionStyle -eq 'RAW') -or (Test-DriveLetterInUse -DriveLetter $disk | Where-Object { $_.DriveLetter })) {
-        Write-Host "Skipping initialization for Disk $diskNumber (Already initialized or has a drive letter)."
-        continue
-    }
-
-    # Check if the disk is already formatted
-    if ($disk | Get-Partition | Where-Object { $_.DriveLetter }) {
-        $existingDriveLetter = ($disk | Get-Partition | Where-Object { $_.DriveLetter }).DriveLetter
-        Write-Host "Disk $diskNumber is already formatted with drive letter $existingDriveLetter. Skipping initialization."
-    }
-    else {
-        # Initialize the disk with GPT partition style
-        Initialize-Disk -Number $diskNumber -PartitionStyle GPT
-        Write-Host "Disk $diskNumber initialized."
-    }
-}
-
-# Format the volumes with NTFS file system and specific label
-foreach ($diskNumber in $diskNumbers) {
-    # Skip Disk 0 (OS disk)
-    if ($diskNumber -eq 0) {
-        Write-Host "Skipping formatting for Disk 0 (OS disk)."
-        continue
-    }
-
-    $disk = Get-Disk -Number $diskNumber
 
     # Skip if the disk already has a drive letter
     if (Test-DriveLetterInUse -DriveLetter ($disk | Get-Partition | Where-Object { $_.DriveLetter })) {
-        Write-Host "Skipping formatting for Disk $diskNumber (Already has a drive letter)."
+        Write-Host "Skipping partition creation for Disk $diskNumber (Already has a drive letter)."
         continue
     }
 
-    # Check if the disk is already formatted
-    if ($disk | Get-Partition | Where-Object { $_.DriveLetter }) {
-        $existingDriveLetter = ($disk | Get-Partition | Where-Object { $_.DriveLetter }).DriveLetter
-        Write-Host "Disk $diskNumber is already formatted with drive letter $existingDriveLetter. Skipping formatting."
+    $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
+
+    if (Test-DriveLetterInUse -DriveLetter $nextAvailableDriveLetter) {
+        Write-Host "Drive letter $nextAvailableDriveLetter is already in use. Skipping partition creation for Disk $diskNumber."
     }
     else {
-        $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
+        # Format the volume with NTFS file system and specific label
+        Format-Volume -DriveLetter $nextAvailableDriveLetter -FileSystem NTFS -NewFileSystemLabel "SC1CALLS $diskNumber" -AllocationUnitSize 65536 -Confirm:$false
 
-        if (Test-DriveLetterInUse -DriveLetter $nextAvailableDriveLetter) {
-            Write-Host "Drive letter $nextAvailableDriveLetter is already in use for Disk $diskNumber. Skipping formatting."
-        }
-        else {
-            New-Partition -DiskNumber $diskNumber -UseMaximumSize -DriveLetter $nextAvailableDriveLetter
-            Write-Host "Partition on Disk $diskNumber created with drive letter $nextAvailableDriveLetter."
-            
-            Format-Volume -DriveLetter $nextAvailableDriveLetter -FileSystem NTFS -NewFileSystemLabel "SC1CALLS $diskNumber" -AllocationUnitSize 65536 -Confirm:$false
-            Write-Host "Formatted volume with drive letter $nextAvailableDriveLetter and label SC1CALLS $diskNumber."
-        }
+        # Initialize the disk with GPT partition style
+        Initialize-Disk -Number $diskNumber -PartitionStyle GPT
+        Write-Host "Disk $diskNumber initialized and partition on Disk $diskNumber created with drive letter $nextAvailableDriveLetter."
     }
 }
