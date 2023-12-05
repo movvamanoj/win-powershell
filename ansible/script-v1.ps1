4dae29ab0a07f5d32a12d107975fc90b1ea64d56
movvamanoj <movvamanoj@gmail.com>
2023-12-05 15:26:33 +0530

    added script-v1 file ps1 sleep 30sec

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -1,29 +1,23 @@
-# Dynamically retrieve all disk numbers, including offline disks
-$diskNumbers = Get-Disk | Select-Object -ExpandProperty Number
-
-# Function to get the next available drive letter
-function Get-NextAvailableDriveLetter {
-    $usedDriveLetters = Get-Partition | Where-Object { $_.DriveType -eq 'Fixed' } | Select-Object -ExpandProperty DriveLetter
-    $alphabet = [char[]]('D'..'Z')
-   
-    foreach ($letter in $alphabet) {
-        if ($usedDriveLetters -notcontains $letter) {
-            return $letter
-        }
-    }
-
-    throw "No available drive letters found."
-}
-
 # Check if each disk is already initialized
 foreach ($diskNumber in $diskNumbers) {
     $disk = Get-Disk -Number $diskNumber
 
     # Check if the disk is offline
     if ($disk.IsOffline) {
-        # Bring the disk online
-        Start-Process -Wait -FilePath "C:\Windows\System32\diskpart.exe" -ArgumentList "/s C:\Temp\online-diskpart-script.txt"
-        Write-Host "Disk $diskNumber is brought online."
+        # Try to clear the read-only attribute to bring the disk online
+        Clear-Disk -Number $diskNumber -RemoveData -Confirm:$false
+        Write-Host "Attempting to bring Disk $diskNumber online."
+        
+        # Wait for a moment to ensure the disk is online
+        Start-Sleep -Seconds 5
+
+        # Check if the disk is still offline
+        $disk = Get-Disk -Number $diskNumber
+        if ($disk.IsOffline) {
+            Write-Host "Failed to bring Disk $diskNumber online."
+        } else {
+            Write-Host "Disk $diskNumber is brought online."
+        }
     }
 
     # Check if the disk is not initialized
@@ -34,24 +28,5 @@ foreach ($diskNumber in $diskNumbers) {
     }
     else {
         Write-Host "Disk $diskNumber is already initialized. Skipping initialization."
-    }
-}
-
-# Create a new partition on each disk with specific drive letters and volume label
-foreach ($diskNumber in $diskNumbers) {
-    $driveLetterInUse = Get-Partition -DiskNumber $diskNumber | Get-Volume | Select-Object -ExpandProperty DriveLetter
-
-    if ($driveLetterInUse) {
-        Write-Host "Drive letter $driveLetterInUse is already in use for Disk $diskNumber. Skipping partition creation."
-        # Display current drive letter and label
-        $currentVolume = Get-Volume -DriveLetter $driveLetterInUse
-        Write-Host "Current Drive Letter: $($currentVolume.DriveLetter), Label: $($currentVolume.FileSystemLabel)"
-    }
-    else {
-        $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
-        New-Partition -DiskNumber $diskNumber -UseMaximumSize -AssignDriveLetter
-        $volumeLabel = "SC1CALL$($diskNumber)"
-        Format-Volume -DriveLetter $nextAvailableDriveLetter -FileSystem NTFS -NewFileSystemLabel $volumeLabel -AllocationUnitSize 65536 -ErrorAction Stop
-        Write-Host "Partition on Disk $diskNumber created with drive letter $nextAvailableDriveLetter and label $volumeLabel."
     }
 }
