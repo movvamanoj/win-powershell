94774cd883ea4662be380de38657638fc40dafb5
movvamanoj <movvamanoj@gmail.com>
2023-12-05 15:29:23 +0530

    added script-v1 file ps1 sleep 30sec

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -1,27 +1,26 @@
+# Dynamically retrieve all disk numbers
+$diskNumbers = Get-Disk | Where-Object { $_.IsOffline -eq $false } | Select-Object -ExpandProperty Number
+
+# Function to get the next available drive letter
+function Get-NextAvailableDriveLetter {
+    $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
+    $alphabet = [char[]]('D'..'Z')
+   
+    foreach ($letter in $alphabet) {
+        if ($usedDriveLetters -notcontains $letter) {
+            return $letter
+        }
+    }
+
+    throw "No available drive letters found."
+}
+
 # Check if each disk is already initialized
 foreach ($diskNumber in $diskNumbers) {
     $disk = Get-Disk -Number $diskNumber
 
-    # Check if the disk is offline
-    if ($disk.IsOffline) {
-        # Try to clear the read-only attribute to bring the disk online
-        Clear-Disk -Number $diskNumber -RemoveData -Confirm:$false
-        Write-Host "Attempting to bring Disk $diskNumber online."
-        
-        # Wait for a moment to ensure the disk is online
-        Start-Sleep -Seconds 5
-
-        # Check if the disk is still offline
-        $disk = Get-Disk -Number $diskNumber
-        if ($disk.IsOffline) {
-            Write-Host "Failed to bring Disk $diskNumber online."
-        } else {
-            Write-Host "Disk $diskNumber is brought online."
-        }
-    }
-
-    # Check if the disk is not initialized
-    if ($disk.PartitionStyle -eq 'RAW') {
+    # Check if the disk is not online or not initialized
+    if ($disk.IsOffline -or ($disk.PartitionStyle -eq 'RAW')) {
         # Initialize the disk with GPT partition style
         Initialize-Disk -Number $diskNumber -PartitionStyle GPT
         Write-Host "Disk $diskNumber initialized."
@@ -29,4 +28,27 @@ foreach ($diskNumber in $diskNumbers) {
     else {
         Write-Host "Disk $diskNumber is already initialized. Skipping initialization."
     }
+}
+
+# Create a new partition on each disk with specific drive letters
+$nextAvailableDriveLetters = @()
+
+foreach ($diskNumber in $diskNumbers) {
+    $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
+    $nextAvailableDriveLetters += $nextAvailableDriveLetter
+
+    $driveLetterInUse = Get-Volume -DriveLetter $nextAvailableDriveLetter -ErrorAction SilentlyContinue
+
+    if ($driveLetterInUse) {
+        Write-Host "Drive letter $nextAvailableDriveLetter is already in use for Disk $diskNumber. Skipping partition creation."
+    }
+    else {
+        New-Partition -DiskNumber $diskNumber -UseMaximumSize -DriveLetter $nextAvailableDriveLetter
+        Write-Host "Partition on Disk $diskNumber created with drive letter $nextAvailableDriveLetter."
+    }
+}
+
+# Format the volumes with the NTFS file system
+for ($i = 0; $i -lt $diskNumbers.Count; $i++) {
+    Format-Volume -DriveLetter $nextAvailableDriveLetters[$i] -FileSystem NTFS -NewFileSystemLabel "SC1CALLS $i" -AllocationUnitSize 65536 -ErrorAction Stop
 }
