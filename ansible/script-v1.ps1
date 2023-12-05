27581eabe5252b61a59678727df8b44c3b9b356c
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-05 17:14:51 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -6,7 +6,8 @@ $osDiskNumber = (Get-WmiObject -Class Win32_DiskDrive | Where-Object { $_.Device
 # Function to get the next available drive letter
 function Get-NextAvailableDriveLetter {
     $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
-    $alphabet = [char[]]('D'..'Z')
+    $alphabet = 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B'
+
    
     foreach ($letter in $alphabet) {
         if ($usedDriveLetters -notcontains $letter) {
