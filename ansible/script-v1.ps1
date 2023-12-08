240e280fc40506058fa46024f65d18ca6a03ba0b
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-08 19:46:05 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -5,13 +5,18 @@ $volumes = Get-Volume
 $targetVolume = $volumes | Where-Object { $_.DriveLetter -eq 'G' }
 
 if ($targetVolume) {
-    try {
-        # Attempt to change the drive letter to P
-        $targetVolume | Set-Volume -NewDriveLetter P -Confirm:$false
+    # Check if the volume is formatted
+    if ($targetVolume.FileSystemType -ne 'RAW') {
+        try {
+            # Attempt to change the drive letter to P
+            $targetVolume | Set-Volume -NewDriveLetter P -Confirm:$false
 
-        Write-Host "Drive letter changed from G to P successfully."
-    } catch {
-        Write-Host "Error changing drive letter: $_"
+            Write-Host "Drive letter changed from G to P successfully."
+        } catch {
+            Write-Host "Error changing drive letter: $_"
+        }
+    } else {
+        Write-Host "The volume is not formatted. Cannot change the drive letter."
     }
 } else {
     Write-Host "No volume with drive letter G found."
