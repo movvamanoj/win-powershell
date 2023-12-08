2a328849eef2fe99db71b50e5e86ae4d983fb055
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-08 15:53:40 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -29,45 +29,7 @@ function Test-DriveLetterInUse {
     return $usedDriveLetters -contains $DriveLetter
 }
 
-# ... (previous code)
-
-# Check if any existing disk has drive letter 'G' and change it to 'P' if there's enough available capacity
-foreach ($diskNumber in $diskNumbers) {
-    # Skip Disk 0 (OS disk)
-    if ($diskNumber -eq 0) {
-        Write-Host "Skipping drive letter check for Disk 0 (OS disk)."
-        continue
-    }
-
-    # Check if the disk already has a drive letter 'G'
-    if ($diskNumber -in $diskNumbersLetter.Keys -and 'G' -in $diskNumbersLetter[$diskNumber]) {
-        Write-Host "Drive letter 'G' found for Disk $diskNumber. Checking available capacity."
-
-        $disk = Get-Disk -Number $diskNumber
-
-        # Check if there's enough available capacity to create a new partition
-        if ($disk | Where-Object { $_.FreeSpace -ge 1GB }) {
-            Write-Host "Enough available capacity on Disk $diskNumber. Changing drive letter 'G' to 'P'."
-
-            # Change drive letter 'G' to 'P'
-            $partition = Get-Partition -DiskNumber $diskNumber | Where-Object { $_.DriveLetter -eq 'G' }
-
-            if ($partition) {
-                $partition | Set-Partition -NewDriveLetter 'P'
-                Write-Host "Drive letter 'G' changed to 'P' for Disk $diskNumber."
-                $diskNumbersLetter[$diskNumber] += 'P'
-            }
-            else {
-                Write-Host "Partition with drive letter 'G' not found on Disk $diskNumber. Skipping drive letter change."
-            }
-        }
-        else {
-            Write-Host "Not enough available capacity on Disk $diskNumber to change drive letter 'G' to 'P'."
-        }
-    }
-}
-
-# ... (rest of the code)
+# ...
 
 # Check if each disk is already initialized and has a drive letter
 foreach ($diskNumber in $diskNumbers) {
@@ -96,6 +58,27 @@ foreach ($diskNumber in $diskNumbers) {
 
     # Add the disk number to the diskNumbersLetter with an empty array for drive letters
     $diskNumbersLetter[$diskNumber] = @()
+
+    # Check if the disk has drive letter G and change it to P
+    if ($diskNumber -in $diskNumbersLetter.Keys -and 'G' -in $diskNumbersLetter[$diskNumber]) {
+        # Change drive letter from G to P
+        $partition = Get-Partition -DiskNumber $diskNumber | Where-Object { $_.DriveLetter -eq 'G' }
+
+        if ($partition) {
+            $partition | Set-Partition -NewDriveLetter $desiredDriveLetter
+            Write-Host "Drive letter on Disk $diskNumber changed from G to $desiredDriveLetter."
+            $diskNumbersLetter[$diskNumber] = $diskNumbersLetter[$diskNumber] -replace 'G', $desiredDriveLetter
+
+            # Refresh the disk information
+            $disk = Get-Disk -Number $diskNumber
+            if ($disk.IsOffline) {
+                Online-Disk -Number $diskNumber
+            }
+        }
+        else {
+            Write-Host "Partition with drive letter G not found on Disk $diskNumber. Skipping drive letter change."
+        }
+    }
 }
 
 # Create a new partition on each disk with specific drive letters
