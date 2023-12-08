62ff57d1cb90e0546e78218f45443cec5f620da3
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-08 17:29:52 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -10,7 +10,7 @@ $diskNumbersLetter = @{}
 # Function to get the next available drive letter
 function Get-NextAvailableDriveLetter {
     $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
-    $alphabet = 'G'#, 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    $alphabet = 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 
     foreach ($letter in $alphabet) {
         if ($usedDriveLetters -notcontains $letter) {
@@ -49,11 +49,36 @@ foreach ($diskNumberToChange in $diskNumbers) {
         Write-Host "Disk $diskNumberToChange does not have drive letter G. Skipping drive letter change."
     }
 }
+
 # Continue with other processes (e.g., initialization, partition creation, formatting) as usual
-# ...
+foreach ($diskNumber in $diskNumbers) {
+    # Skip Disk 0 (OS disk)
+    if ($diskNumber -eq 0) {
+        Write-Host "Skipping initialization for Disk 0 (OS disk)."
+        continue
+    }
 
+    # Skip if the disk is already initialized or has a drive letter
+    if ($diskNumber -in $diskNumbersLetter.Keys) {
+        Write-Host "Skipping initialization for Disk $diskNumber (Already initialized or has a drive letter)."
+        continue
+    }
 
-# Create a new partition on each disk with specific drive letters
+    $disk = Get-Disk -Number $diskNumber
+
+    # Check if the disk is already initialized
+    if ($disk.IsOffline -or ($disk.PartitionStyle -eq 'RAW')) {
+        Initialize-Disk -Number $diskNumber -PartitionStyle GPT
+        Write-Host "Disk $diskNumber initialized."
+    }
+    else {
+        Write-Host "Disk $diskNumber is already initialized. Skipping initialization."
+    }
+
+    # Add the disk number to the diskNumbersLetter with an empty array for drive letters
+    $diskNumbersLetter[$diskNumber] = @()
+}
+
 foreach ($diskNumber in $diskNumbers) {
     # Skip Disk 0 (OS disk)
     if ($diskNumber -eq 0) {
@@ -70,7 +95,7 @@ foreach ($diskNumber in $diskNumbers) {
     $nextAvailableDriveLetter = Get-NextAvailableDriveLetter
 
     # Check if the drive letter is already in use
-    if (Test-DriveLetterInUse -DiskNumber $diskNumber -DriveLetter $nextAvailableDriveLetter) {
+    if ($diskNumber -in $diskNumbersLetter.Keys -and $nextAvailableDriveLetter -in $diskNumbersLetter[$diskNumber]) {
         Write-Host "Drive letter $nextAvailableDriveLetter is already in use for Disk $diskNumber. Skipping partition creation."
     }
     else {
@@ -90,9 +115,7 @@ foreach ($diskNumber in $diskNumbers) {
 
     foreach ($driveLetter in $diskNumbersLetter[$diskNumber]) {
         # Check if the partition exists before formatting
-        $partitionToFormat = Get-Partition -DiskNumber $diskNumber | Where-Object { $_.DriveLetter -eq $driveLetter }
-
-        if ($partitionToFormat) {
+        if (Get-Partition -DiskNumber $diskNumber | Where-Object { $_.DriveLetter -eq $driveLetter }) {
             Format-Volume -DriveLetter $driveLetter -FileSystem NTFS -NewFileSystemLabel "SC1CALLS" -AllocationUnitSize 65536 -Confirm:$false
             Write-Host "Formatted volume with drive letter $driveLetter and label SC1CALLS."
         }
