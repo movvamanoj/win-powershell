e7c61f2d9dffcc7ba2b6f8e9711e3ef48f189d58
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-08 14:05:05 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -1,15 +1,6 @@
 # Specify the disk numbers
 $diskNumbers = (Get-Disk).Number
 
-# Function to check if the drive letter 'G' is present for a specific disk
-function Test-DriveLetterGPresent {
-    param (
-        [string[]]$DriveLetters
-    )
-
-    return $DriveLetters -contains 'G'
-}
-
 # Function to get the next available drive letter
 function Get-NextAvailableDriveLetter {
     $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
@@ -24,53 +15,38 @@ function Get-NextAvailableDriveLetter {
     throw "No available drive letters found."
 }
 
-# Check if any disk has the letter "G" and update it to "P" in $diskNumbersLetter
-foreach ($diskNumber in $diskNumbers) {
-    # Check if $diskNumbersLetter has an entry for the current disk number
-    if ($diskNumbersLetter.ContainsKey($diskNumber)) {
-        $driveLetters = $diskNumbersLetter[$diskNumber]
+# ...
 
-        # Check if "G" is present in the drive letters for the current disk
-        if (Test-DriveLetterGPresent -DriveLetters $driveLetters) {
-            foreach ($driveLetter in $driveLetters) {
-                if ($driveLetter -eq 'G') {
-                    # Change the drive letter from "G" to "P" without formatting
-                    Set-Partition -DriveLetter 'G' -NewDriveLetter 'P' -NoFormatting -Confirm:$false
+# Loop through each disk number and check if 'G' is present in the drive letters
+foreach ($diskNumber in $diskNumbersLetter.Keys) {
+    $driveLetters = $diskNumbersLetter[$diskNumber]
 
-                    # Update $diskNumbersLetter with the new drive letter
-                    $diskNumbersLetter[$diskNumber] = $diskNumbersLetter[$diskNumber] -replace 'G', 'P'
+    if ($driveLetters -contains 'G') {
+        # Change 'G' to 'P'
+        $newDriveLetters = $driveLetters -replace 'G', 'P'
 
-                    # Output a message about the change
-                    Write-Host "Drive letter for Disk $diskNumber changed from 'G' to 'P'. No formatting performed."
-                }
-            }
+        # Update the stored drive letters with the modified ones
+        $diskNumbersLetter[$diskNumber] = $newDriveLetters
 
-            # Continue with the rest of the code for the specific disk
-            # You can add the remaining code here or call other functions as needed
+        Write-Host "Drive letter 'G' found for Disk $diskNumber. Changing it to 'P' without formatting."
+
+        # Get the partition information for the drive letter 'G' on the current disk
+        $partition = Get-Partition -DiskNumber $diskNumber | Where-Object { $_.DriveLetter -eq 'G' }
+
+        if ($partition) {
+            # Set the new drive letter 'P' for the partition
+            Set-Partition -InputObject $partition -NewDriveLetter 'P'
+            Write-Host "Drive letter changed to 'P' for Disk $diskNumber without formatting."
+
+            # Remove 'G' from the list of drive letters for the current disk
+            $diskNumbersLetter[$diskNumber] = $newDriveLetters -ne 'G'
         } else {
-            # Output a message indicating that "G" is not present for the current disk
-            Write-Host "Drive letter 'G' not present for Disk $diskNumber. Skipping modification and continuing with the rest of the code."
+            Write-Host "Partition with drive letter 'G' not found on Disk $diskNumber. Skipping drive letter change."
         }
-    } else {
-        # Output a message indicating that there is no entry for the current disk number
-        Write-Host "No entry found for Disk $diskNumber in $diskNumbersLetter. Skipping modification and continuing with the rest of the code."
     }
 }
 
-
-# Function to get the next available drive letter
-function Get-NextAvailableDriveLetter {
-    $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
-    $alphabet = 'G'
-    
-    foreach ($letter in $alphabet) {
-        if ($usedDriveLetters -notcontains $letter) {
-            return $letter
-        }
-    }
-
-    throw "No available drive letters found."
-}
+# ...
 
 # Create a new partition on each disk with specific drive letters
 foreach ($diskNumber in $diskNumbers) {
