1d226589ea408ff4a95c578588602ee7eaa098ba
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-08 19:42:34 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -1,39 +1,18 @@
-# Specify the desired drive letter
-$desiredDriveLetter = 'P'
+# Get information about volumes
+$volumes = Get-Volume
 
-# Get all disks except the OS disk (Disk 0)
-$disks = Get-Disk | Where-Object { $_.Number -gt 0 }
+# Check if there is a volume with drive letter G
+$targetVolume = $volumes | Where-Object { $_.DriveLetter -eq 'G' }
 
-# Define a function to change drive letter
-function Change-DriveLetterTo {
-  param (
-    [Parameter(Mandatory = $true)]
-    [object] $partition,
-    [string] $newDriveLetter
-  )
+if ($targetVolume) {
+    try {
+        # Attempt to change the drive letter to P
+        $targetVolume | Set-Volume -NewDriveLetter P -Confirm:$false
 
-  Set-Partition -DriveLetter $newDriveLetter -Confirm:$false -InputObject $partition
-}
-
-# Loop through each disk
-foreach ($disk in $disks) {
-  # Check for partitions with drive letter G
-  $partitionsWithDriveG = $disk.Partitions | Where-Object { $_.DriveLetter -eq 'G' }
-
-  # If any partition has G, change it and all others to desired letter
-  if ($partitionsWithDriveG) {
-    Write-Host "Changing drive letter for disk $disk.Number from G to $desiredDriveLetter..."
-
-    foreach ($partition in $disk.Partitions) {
-      Change-DriveLetterTo -partition $partition -newDriveLetter $desiredDriveLetter
+        Write-Host "Drive letter changed from G to P successfully."
+    } catch {
+        Write-Host "Error changing drive letter: $_"
     }
-
-    Write-Host "Drive letter changed successfully."
-    break
-  }
-}
-
-# Report if no disk with G was found
-if (!$partitionsWithDriveG) {
-  Write-Host "No disk found with drive letter G."
+} else {
+    Write-Host "No volume with drive letter G found."
 }
