b245538947f8fbff472c1929422c89fe44a39a6a
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-08 19:48:26 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -1,23 +1,17 @@
-# Get information about volumes
-$volumes = Get-Volume
+# Get all disks
+$disks = Get-Disk | Where-Object { $_.DriveLetter -ne $null }
 
-# Check if there is a volume with drive letter G
-$targetVolume = $volumes | Where-Object { $_.DriveLetter -eq 'G' }
+# Find the disk with letter G
+$gDisk = $disks | Where-Object { $_.DriveLetter -eq "G" }
 
-if ($targetVolume) {
-    # Check if the volume is formatted
-    if ($targetVolume.FileSystemType -ne 'RAW') {
-        try {
-            # Attempt to change the drive letter to P
-            $targetVolume | Set-Volume -NewDriveLetter P -Confirm:$false
+# Check if G exists
+if ($gDisk) {
+  # Get the disk number
+  $diskNumber = $gDisk.Number
 
-            Write-Host "Drive letter changed from G to P successfully."
-        } catch {
-            Write-Host "Error changing drive letter: $_"
-        }
-    } else {
-        Write-Host "The volume is not formatted. Cannot change the drive letter."
-    }
+  # Change the drive letter to P
+  Set-Partition -DriveLetter "G" -NewDriveLetter "P" -DiskNumber $diskNumber
+  Write-Host "Disk G successfully changed to P."
 } else {
-    Write-Host "No volume with drive letter G found."
+  Write-Host "Disk G not found."
 }
