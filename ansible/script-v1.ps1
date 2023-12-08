fbd1ad30fafe2d1f319db80c5375560b42367d6a
Manojkumar Movva <movvamanoj@gmail.com>
2023-12-08 13:06:30 +0530

    Update script-v1.ps1

--- ansible/script-v1.ps1
+++ ansible/script-v1.ps1
@@ -1,14 +1,20 @@
 # Specify the disk numbers
 $diskNumbers = (Get-Disk).Number
 
-# Create a variable to store allocated disk letters along with their disk numbers
-$diskNumbersLetter = @{}
+# Function to check if the drive letter 'G' is present for a specific disk
+function Test-DriveLetterGPresent {
+    param (
+        [string[]]$DriveLetters
+    )
+
+    return $DriveLetters -contains 'G'
+}
 
 # Function to get the next available drive letter
 function Get-NextAvailableDriveLetter {
     $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
     $alphabet = 'G'
-
+    
     foreach ($letter in $alphabet) {
         if ($usedDriveLetters -notcontains $letter) {
             return $letter
@@ -18,51 +24,51 @@ function Get-NextAvailableDriveLetter {
     throw "No available drive letters found."
 }
 
-# Function to test if a drive letter is in use for a specific disk
-function Test-DriveLetterInUse {
-    param (
-        [int]$DiskNumber,
-        [string]$DriveLetter
-    )
+# Create a variable to store allocated disk letters along with their disk numbers
+$diskNumbersLetter = @{}
 
-    $usedDriveLetters = $diskNumbersLetter[$DiskNumber]
-    return $usedDriveLetters -contains $DriveLetter
-}
-
-# Check if each disk is already initialized and has a drive letter
+# Check if any disk has the letter "G" and update it to "P" in $diskNumbersLetter
 foreach ($diskNumber in $diskNumbers) {
-    # Skip Disk 0 (OS disk)
-    if ($diskNumber -eq 0) {
-        Write-Host "Skipping initialization for Disk 0 (OS disk)."
-        continue
-    }
+    $driveLetters = $diskNumbersLetter[$diskNumber]
 
-    # Skip if the disk is already initialized or has a drive letter
-    if ($diskNumber -in $diskNumbersLetter.Keys) {
-        Write-Host "Skipping initialization for Disk $diskNumber (Already initialized or has a drive letter)."
-        continue
-    }
+# Check if any disk has the letter "G" and update it to "P" in $diskNumbersLetter
+foreach ($diskNumber in $diskNumbers) {
+    $driveLetters = $diskNumbersLetter[$diskNumber]
 
-    $disk = Get-Disk -Number $diskNumber
+    # Check if "G" is present in the drive letters for the current disk
+    if (Test-DriveLetterGPresent -DriveLetters $driveLetters) {
+        foreach ($driveLetter in $driveLetters) {
+            if ($driveLetter -eq 'G') {
+                # Change the drive letter from "G" to "P" without formatting
+                Set-Partition -DriveLetter 'G' -NewDriveLetter 'P' -NoFormatting -Confirm:$false
 
-    # Check if the disk is already initialized
-    if ($disk.IsOffline -or ($disk.PartitionStyle -eq 'RAW')) {
-        Initialize-Disk -Number $diskNumber -PartitionStyle GPT
-        Write-Host "Disk $diskNumber initialized."
+                # Update $diskNumbersLetter with the new drive letter
+                $diskNumbersLetter[$diskNumber] = $diskNumbersLetter[$diskNumber] -replace 'G', 'P'
+
+                # Output a message about the change
+                Write-Host "Drive letter for Disk $diskNumber changed from 'G' to 'P'. No formatting performed."
+            }
+        }
     }
     else {
-        Write-Host "Disk $diskNumber is already initialized. Skipping initialization."
+        # Output a message indicating that "G" is not present for the current disk
+        Write-Host "Drive letter 'G' not present for Disk $diskNumber. Skipping modification and continuing with the rest of the code."
+    }
+}
+
+
+# Function to get the next available drive letter
+function Get-NextAvailableDriveLetter {
+    $usedDriveLetters = Get-Volume | Select-Object -ExpandProperty DriveLetter
+    $alphabet = 'G'
+    
+    foreach ($letter in $alphabet) {
+        if ($usedDriveLetters -notcontains $letter) {
+            return $letter
+        }
     }
 
-    # Add the disk number to the diskNumbersLetter with an empty array for drive letters
-    $diskNumbersLetter[$diskNumber] = @(Get-Partition -DiskNumber $diskNumber | Select-Object -ExpandProperty DriveLetter)
-
-    # Check if the disk has drive letter G
-    if ($diskNumbersLetter[$diskNumber] -contains 'G') {
-        Write-Host "Changing drive letter G to P for Disk $diskNumber."
-        Set-Partition -DiskNumber $diskNumber -DriveLetter 'G' -NewDriveLetter 'P' -NoFormatting -Confirm:$false
-        $diskNumbersLetter[$diskNumber] = @(Get-Partition -DiskNumber $diskNumber | Select-Object -ExpandProperty DriveLetter)
-    }
+    throw "No available drive letters found."
 }
 
 # Create a new partition on each disk with specific drive letters
